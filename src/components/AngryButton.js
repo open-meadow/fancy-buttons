3085fb98965894dc7b40827bf131b13c2cab4394
open-meadow <rohan.varughese@protonmail.com>
2023-01-09 15:10:01 +0000

    Initialize components

---
+++ src/components/AngryButton.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const AngryButton = () => {
+  return <button className="AngryButton">
+    {/* When the threshold is not reached */}<span>Don't click me too much! </span>
+    {/* When the threshold is not reached */}<span>Rawr!</span>
+  </button>
+}
+
+export default AngryButton;
---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LightSwitchButton = () => {
+  return <button className="LightSwitchButton">
+    {/* When the state is on */}
+    <span>
+      <i>💡</i> I'm on!
+    </span>
+    {/* When the state is off */}
+    <span className="off">
+      <i>💡</i> I'm off!
+    </span>
+  </button>;
+};
+
+export default LightSwitchButton;
